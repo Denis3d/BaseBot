bde69c0a5e67babe8e114a0cb539c50cbdc596f8
DenisDd <dd.denis2544@gmail.com>
2019-04-21 20:25:29 +0100

    Add multiple guild support in welcome message

--- extensions/welcome_message.py
+++ extensions/welcome_message.py
@@ -22,17 +22,17 @@ class Welcome_message(commands.Cog):
         self.bot: commands.Bot = bot
         if not os.path.isfile('./config/welcome_message.json'):
             with open('./config/welcome_message.json', 'w+') as outfile:
-                data = {'welcome_message': "Welcome to {member_name}", 'embed_color': {'r': 255, 'g': 0, 'b': 0}, 'default_role': None, 'welcome_message_channel': None}
+                data = {'insert_guild_id_here': {'welcome_message': "Welcome to {member_name}", 'embed_color': {'r': 255, 'g': 0, 'b': 0}, 'default_role': None, 'welcome_message_channel': None}}
                 json.dump(data, outfile, indent=4)
         self.config = json.load(open('./config/welcome_message.json', 'r'))
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         embed = discord.Embed(
-            colour=discord.Colour.from_rgb(self.config['embed_color']['r'], self.config['embed_color']['g'],
-                                           self.config['embed_color']['b']),
-            description=utils.format_message(self.config['welcome_message'], bot=self.bot, member=member))
-        await member.guild.get_channel(self.config['welcome_message_channel']).send(embed=embed) if member.guild.get_channel(self.config['welcome_message_channel']) else print("Wrong channel id in config file")
+            colour=discord.Colour.from_rgb(self.config[str(member.guild.id)]['embed_color']['r'], self.config[str(member.guild.id)]['embed_color']['g'],
+                                           self.config[str(member.guild.id)]['embed_color']['b']),
+            description=utils.format_message(self.config[str(member.guild.id)]['welcome_message'], bot=self.bot, member=member))
+        await member.guild.get_channel(self.config[str(member.guild.id)]['welcome_message_channel']).send(embed=embed) if member.guild.get_channel(self.config[str(member.guild.id)]['welcome_message_channel']) else print("Wrong channel id in config file")
 
-        if self.config['default_role'] is not None:
-            await member.add_roles(member.guild.get_role(self.config['default_role']))
+        if self.config[str(member.guild.id)]['default_role'] is not None:
+            await member.add_roles(member.guild.get_role(self.config[str(member.guild.id)]['default_role']))
